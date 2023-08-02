f89f92db3e61a46c320cc21b2d14446bc9e9a344
Muhammadrasul <holturaevm@gmail.com>
2023-08-02 21:43:19 +0300

    work on adaptive

--- src/components/Advantages/AdvantageCard.jsx
+++ src/components/Advantages/AdvantageCard.jsx
@@ -2,10 +2,12 @@ import TargetIcon from "../../assets/icons/TargetIcon";
 
 const AdvantageCard = ({ title, txt }) => {
   return (
-    <div className="flex space-between max-w-[370px] bg-[#F5F5F5] min-h-[200px] rounded-sm">
+    <div className="flex space-between max-w-[370px] bg-[#F5F5F5] md:min-h-[200px] min-h-[145px] rounded-sm">
       <div className=" p-5 gap-2 flex flex-col">
-        <p className="text-xl text-[#24201F] font-semibold">{title}</p>
-        <p>{txt}</p>
+        <p className="md:text-xl text-base text-[#24201F] font-semibold">
+          {title}
+        </p>
+        <p className="md:text-[16px] text-sm">{txt}</p>
       </div>
       <div className="flex items-end w-[100px]">
         <TargetIcon />
