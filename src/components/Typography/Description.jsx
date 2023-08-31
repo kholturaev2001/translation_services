18c7c33d0ca9f972dbace8f61a898fb0421370dd
Muhammadrasul <holturaevm@gmail.com>
2023-08-31 09:24:46 +0300

    work on services => valuation

--- src/components/Typography/Description.jsx
+++ src/components/Typography/Description.jsx
@@ -1,7 +1,7 @@
 const Description = ({ children }) => {
   return (
     <div className="mx-3">
-      <p className="my-6 mx-auto md:text-base text-sm md:max-w-[1000px] max-w-[100%]">
+      <p className="mt-5 mb-4 mx-auto md:text-base text-sm md:max-w-[1000px] max-w-[100%]">
         {children}
       </p>
     </div>
---
+++ src/components/Typography/Title3.jsx
@@ -0,0 +1,12 @@
+const Title3 = ({ children }) => {
+    return (
+      <div className="mx-3">
+        <p className="md:text-base text-sm font-semibold mx-auto md:max-w-[1000px] max-w-[100%]">
+          {children}
+        </p>
+      </div>
+    );
+  };
+  
+  export default Title3;
+  
