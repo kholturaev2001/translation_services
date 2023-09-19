149aeb36393b242f48b6643cd0bc296b0f12f9aa
Muhammadrasul <holturaevm@gmail.com>
2023-09-19 13:56:48 +0300

    add table

--- src/components/DTable.jsx
+++ src/components/DTable.jsx
@@ -5,7 +5,6 @@ const DTable = ({ columns, dataSource, ...rest }) => {
     <div className="mx-3 my-1">
       <div className="md:max-w-[1000px] max-w-[100%] mx-auto">
         <Table
-          
           {...rest}
           dataSource={dataSource}
           columns={columns}
